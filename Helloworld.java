871659df256f9a03a1b214b8b7f789643bb6c875
shruti1176 <154695504+shruti1176@users.noreply.github.com>
2023-12-26 22:31:39 +0530

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -1,22 +1,5 @@
-public class FibonacciRecursion {
-
+public class HelloWorld {
     public static void main(String[] args) {
-        int n = 30; // You can change the value of 'n' to generate a different number of terms in the series
-        System.out.println("Fibonacci Series up to " + n + " terms:");
-        long startTime = System.nanoTime();
-        for (int i = 0; i < n; i++) {
-            System.out.println(fibonacci(i) + " ");
-        }
-        long endTime = System.nanoTime();
-        System.out.println("Total time taken " + (endTime - startTime));
-    }
-
-    // Recursive method to calculate Fibonacci number at position 'n'
-    public static int fibonacci(int n) {
-        if (n <= 1) {
-            return n;
-        } else {
-            return fibonacci(n - 1) + fibonacci(n - 2);
-        }
+        System.out.println("Hello, World!");
     }
 }
